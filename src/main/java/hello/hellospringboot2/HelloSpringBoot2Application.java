3de9a6cb0e3dc947a7d7fbc5218e983dc212d9c3
limseungchae <tmdco12@naver.com>
2023-06-05 10:31:42 +0900

    Initial commit

---
+++ src/main/java/hello/hellospringboot2/HelloSpringBoot2Application.java
@@ -0,0 +1,13 @@
+package hello.hellospringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBoot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringBoot2Application.class, args);
+    }
+
+}
